5075e63dadd86fe40ff2c31971d5dcd73bdfb78a
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2022-02-05 21:01:46 +0200

    Create abstract factory method and test genre dao for testing usage
    Created DaoFactory, which uses Singleton patterns for returning needed implementation of factory.
    Added GenreDao for testing future use of dao layer.

---
+++ src/main/java/com/a_smart_cookie/dao/DaoFactory.java
@@ -0,0 +1,40 @@
+package com.a_smart_cookie.dao;
+
+import com.a_smart_cookie.dao.mysql.MysqlDaoFactory;
+import com.a_smart_cookie.exception.DaoException;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public abstract class DaoFactory {
+    private static DaoFactory instance;
+    private static final Lock LOCK = new ReentrantLock();
+    private static final String daoFactoryFCN;
+
+    static {
+        daoFactoryFCN = MysqlDaoFactory.class.getName();
+    }
+
+    protected DaoFactory() {
+    }
+
+     public static DaoFactory getInstance() throws DaoException {
+        try {
+            LOCK.lock();
+            if (instance == null) {
+                try {
+                    Class<?> clazz = Class.forName(DaoFactory.daoFactoryFCN);
+                    instance = (DaoFactory) clazz.getDeclaredConstructor().newInstance();
+                } catch (ReflectiveOperationException ex) {
+                    throw new DaoException("Can't obtain the instance DaoFactory with " + daoFactoryFCN + " FCN", ex);
+                }
+            }
+        } finally {
+            LOCK.unlock();
+        }
+        return instance;
+    }
+
+    public abstract GenreDao getGenreDao();
+
+}
