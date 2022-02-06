e38c3960b50bd3c9e7308fc5fde423d9a83466b9
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2022-02-06 19:29:21 +0200

    Test GenreService and CommandContext
    While testing used DI for proper testing, so changed constructor signature for that.
    Added MockMaker file for testing final classes.

--- src/main/java/com/a_smart_cookie/controller/command/CatalogCommand.java
+++ src/main/java/com/a_smart_cookie/controller/command/CatalogCommand.java
@@ -1,7 +1,11 @@
 package com.a_smart_cookie.controller.command;
 
+import com.a_smart_cookie.dao.DaoFactory;
+import com.a_smart_cookie.dao.EntityTransaction;
+import com.a_smart_cookie.dao.mysql.MysqlGenreDao;
 import com.a_smart_cookie.entity.Genre;
 import com.a_smart_cookie.entity.Language;
+import com.a_smart_cookie.exception.DaoException;
 import com.a_smart_cookie.exception.ServiceException;
 import com.a_smart_cookie.service.GenreService;
 import com.a_smart_cookie.service.impl.GenreServiceImpl;
@@ -15,12 +19,12 @@ public final class CatalogCommand extends Command {
     @Override
     public void process() throws ServletException, IOException {
         try {
-            GenreService genreService = new GenreServiceImpl();
+            GenreService genreService = new GenreServiceImpl(DaoFactory.getInstance().getGenreDao(), new EntityTransaction());
             List<Genre> allGenresByLanguage = genreService.findAllGenresByLanguage(Language.UKRAINIAN);
             System.out.println(allGenresByLanguage);
             request.setAttribute("message", allGenresByLanguage.toString());
             forward("catalog");
-        } catch (ServiceException e) {
+        } catch (ServiceException | DaoException e) {
             e.printStackTrace();
         }
     }
