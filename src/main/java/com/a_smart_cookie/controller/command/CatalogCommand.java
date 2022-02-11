a74814f7e076e1795d876518a36139508480b3b2
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2022-02-11 22:50:28 +0200

    Add simple jspf-files for flexible managing jsp pages
    Created jspfs of
    -head
    -header
    -footer
    -page for encoding
    -taglib for using tag libraries.

--- src/main/java/com/a_smart_cookie/controller/command/CatalogCommand.java
+++ src/main/java/com/a_smart_cookie/controller/command/CatalogCommand.java
@@ -7,7 +7,7 @@ import com.a_smart_cookie.entity.Language;
 import com.a_smart_cookie.entity.Publication;
 import com.a_smart_cookie.exception.ServiceException;
 import com.a_smart_cookie.service.PublicationService;
-import com.a_smart_cookie.service.impl.PublicationServiceImpl;
+import com.a_smart_cookie.service.ServiceFactory;
 import com.a_smart_cookie.util.pagination.ItemsPerPage;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,35 +21,60 @@ public class CatalogCommand extends Command {
 	@Override
 	public HttpPath execute(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			ItemsPerPage itemsPerPage = ItemsPerPage.fromString(request.getParameter("limit"));
+			PublicationService publicationService = ServiceFactory.getInstance().getPublicationService();
 
-			int currentPage = 1;
-			String pageNumberParameter = request.getParameter("page");
-			if (pageNumberParameter != null && Integer.parseInt(pageNumberParameter) >= currentPage ) {
-				currentPage = Integer.parseInt(pageNumberParameter);
-			}
+			int itemsPerPage = getItemsPerPage(request);
+			int totalNumberOfPublications = publicationService.getTotalNumberOfPublications();
+			int numberOfPages = getNumberOfPages(itemsPerPage, totalNumberOfPublications);
+			int currentPage = getCurrentPage(request, numberOfPages);
 
 			Language language = Language.fromString(request.getParameter("lang"));
 
-			PublicationService publicationService = new PublicationServiceImpl();
 			List<Publication> publications = publicationService.findLimitedWithOffsetByLanguage(
-					itemsPerPage.getLimit(),
-					itemsPerPage.getLimit() * --currentPage,
+					itemsPerPage,
+					itemsPerPage * (currentPage-1),
 					language);
 
-			if (publications.isEmpty()) {
-				return new HttpPath(WebPath.Command.CATALOG_FIRST_PAGE, HttpHandlerType.SEND_REDIRECT);
-			}
-
-			System.out.println(publications);
 			request.setAttribute("language", language);
 			request.setAttribute("publications", publications);
+			request.setAttribute("numberOfPages", numberOfPages);
+			request.setAttribute("currentPage", currentPage);
+			request.setAttribute("itemsPerPage", itemsPerPage);
 
 			return new HttpPath(WebPath.Page.CATALOG, HttpHandlerType.FORWARD);
+
 		} catch (ServiceException e) {
 			e.printStackTrace();
 		}
+
 		return new HttpPath(WebPath.Page.ERROR, HttpHandlerType.SEND_REDIRECT);
+	}
+
+	private int getItemsPerPage(HttpServletRequest request) {
+		int itemsPerPage;
+		try {
+			itemsPerPage = ItemsPerPage.fromString(request.getParameter("limit")).getLimit();
+		} catch (IllegalArgumentException e) {
+			itemsPerPage = ItemsPerPage.ONE.getLimit();
+		}
+		return itemsPerPage;
+	}
+
+	private int getNumberOfPages(int itemsPerPage, int totalNumberOfPublications) {
+		int numberOfPages = totalNumberOfPublications / itemsPerPage;
+		if (totalNumberOfPublications % itemsPerPage > 0) {
+			++numberOfPages;
+		}
+		return numberOfPages;
+	}
+
+	private int getCurrentPage(HttpServletRequest request, int numberOfPages) {
+		int currentPage = 1;
+		String pageNumberParameter = request.getParameter("page");
+		if (pageNumberParameter != null && (Integer.parseInt(pageNumberParameter) >= currentPage) && (Integer.parseInt(pageNumberParameter) <= numberOfPages)) {
+			currentPage = Integer.parseInt(pageNumberParameter);
+		}
+		return currentPage;
 	}
 
 }
