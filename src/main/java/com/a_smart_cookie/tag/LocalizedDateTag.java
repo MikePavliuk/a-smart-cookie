617e25d1c48fde6b74f306a8e948425d75e702d5
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2022-03-04 20:52:39 +0200

    Extract locale choosing part of code into separate class
    Created class LocaleHandler with method for choosing locale depends on input language.

--- src/main/java/com/a_smart_cookie/tag/LocalizedDateTag.java
+++ src/main/java/com/a_smart_cookie/tag/LocalizedDateTag.java
@@ -1,15 +1,14 @@
 package com.a_smart_cookie.tag;
 
 import com.a_smart_cookie.entity.Language;
+import com.a_smart_cookie.util.LocaleHandler;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.Locale;
 
 /**
  * Custom jstl tag for converting local date into localized display one
- *
  */
 public class LocalizedDateTag {
 
@@ -17,24 +16,16 @@ public class LocalizedDateTag {
 	 * Converts LocalDate object into string representation depends on language.
 	 *
 	 * @param localDate Local date to be displayed.
-	 * @param language Language for choosing proper locale.
+	 * @param language  Language for choosing proper locale.
 	 * @return String localized date.
 	 */
 	public static String convert(LocalDate localDate, Language language) {
-		Locale locale;
 
-		switch (language) {
-			case ENGLISH:
-				locale = Locale.UK;
-				break;
-
-			case UKRAINIAN:
-			default:
-				locale = Locale.forLanguageTag("uk-UA");
-				break;
-		}
-
-		return localDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(locale));
+		return localDate.format(
+				DateTimeFormatter
+						.ofLocalizedDate(FormatStyle.SHORT)
+						.withLocale(LocaleHandler.getLocaleByLanguage(language))
+		);
 	}
 
 }
